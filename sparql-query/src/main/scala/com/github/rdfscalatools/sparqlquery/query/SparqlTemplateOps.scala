d8b8d9477db338f1d31eaf90c597988cfdc530a9
Václav Zeman <prozeman@gmail.com>
2018-05-17 12:41:28 +0200

    Better logging for queries

--- sparql-query/src/main/scala/com/github/rdfscalatools/sparqlquery/query/SparqlTemplateOps.scala
+++ sparql-query/src/main/scala/com/github/rdfscalatools/sparqlquery/query/SparqlTemplateOps.scala
@@ -3,7 +3,9 @@ package com.github.rdfscalatools.sparqlquery.query
 import com.github.rdfscalatools.formats.sparql.SparqlTemplate
 import com.typesafe.scalalogging.Logger
 
-import scala.concurrent.Future
+import scala.concurrent.duration.Duration
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success}
 
 /**
   * Created by Vaclav Zeman on 18. 11. 2017.
@@ -12,24 +14,32 @@ object SparqlTemplateOps {
 
   private val logger = Logger[SparqlTemplateOps.type]
 
-  private def execute[T](operation: QueryOperation, query: SparqlTemplate.Sparql, oneQuery: OneQuery[SparqlTemplate.Sparql, T]): Future[T] = {
+  private def execute[T](operation: QueryOperation, query: SparqlTemplate.Sparql, oneQuery: OneQuery[SparqlTemplate.Sparql, T])(implicit ec: ExecutionContext): Future[T] = {
+    val start = System.nanoTime()
+    val f = oneQuery.execute(operation, query)
     if (logger.underlying.isTraceEnabled) {
-      logger.trace(operation.toString)
-      logger.trace(query.toString())
+      f.onComplete { x =>
+        val duration = Duration.fromNanos(System.nanoTime() - start)
+        val msg = x match {
+          case Success(_) => s"has been successful within ${duration.toMillis} ms."
+          case Failure(th) => s"has been failed within ${duration.toMillis} ms (${th.getClass.getSimpleName}: ${th.getMessage})."
+        }
+        logger.trace(s"Query operation '$operation' $msg\n$query")
+      }
     }
-    oneQuery.execute(operation, query)
+    f
   }
 
   implicit class PimpedSparqlTemplate(query: SparqlTemplate.Sparql) {
-    def select[O, T <: Transaction](implicit queryBuilder: QueryBuilder[SparqlTemplate.Sparql, O, T], transaction: T): Future[O] = execute(QueryOperation.Select, query, queryBuilder(transaction))
+    def select[O, T <: Transaction](implicit queryBuilder: QueryBuilder[SparqlTemplate.Sparql, O, T], transaction: T, ec: ExecutionContext): Future[O] = execute(QueryOperation.Select, query, queryBuilder(transaction))
 
-    def construct[O, T <: Transaction](implicit queryBuilder: QueryBuilder[SparqlTemplate.Sparql, O, T], transaction: T): Future[O] = execute(QueryOperation.Construct, query, queryBuilder(transaction))
+    def construct[O, T <: Transaction](implicit queryBuilder: QueryBuilder[SparqlTemplate.Sparql, O, T], transaction: T, ec: ExecutionContext): Future[O] = execute(QueryOperation.Construct, query, queryBuilder(transaction))
 
-    def describe[O, T <: Transaction](implicit queryBuilder: QueryBuilder[SparqlTemplate.Sparql, O, T], transaction: T): Future[O] = execute(QueryOperation.Describe, query, queryBuilder(transaction))
+    def describe[O, T <: Transaction](implicit queryBuilder: QueryBuilder[SparqlTemplate.Sparql, O, T], transaction: T, ec: ExecutionContext): Future[O] = execute(QueryOperation.Describe, query, queryBuilder(transaction))
 
-    def ask[T <: Transaction](implicit queryBuilder: QueryBuilder[SparqlTemplate.Sparql, Boolean, T], transaction: T): Future[Boolean] = execute(QueryOperation.Ask, query, queryBuilder(transaction))
+    def ask[T <: Transaction](implicit queryBuilder: QueryBuilder[SparqlTemplate.Sparql, Boolean, T], transaction: T, ec: ExecutionContext): Future[Boolean] = execute(QueryOperation.Ask, query, queryBuilder(transaction))
 
-    def update[T <: Transaction](implicit queryBuilder: QueryBuilder[SparqlTemplate.Sparql, Unit, T], transaction: T): Future[Unit] = execute(QueryOperation.Update, query, queryBuilder(transaction))
+    def update[T <: Transaction](implicit queryBuilder: QueryBuilder[SparqlTemplate.Sparql, Unit, T], transaction: T, ec: ExecutionContext): Future[Unit] = execute(QueryOperation.Update, query, queryBuilder(transaction))
   }
 
 }
